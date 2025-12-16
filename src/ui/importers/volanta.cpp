f0e7fb69ef0e51349d37956aa0cd68c1b6a42c58
Ian Parker <ian@geekprojects.com>
2025-12-16 13:23:57 +0000

    Add importer for Volanta exports and related changes

---
+++ src/ui/importers/volanta.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ian Parker on 13/12/2025.
+//
+
+#include "volanta.h"
---
+++ src/ui/importers/volanta.h
@@ -0,0 +1,14 @@
+//
+// Created by Ian Parker on 13/12/2025.
+//
+
+#ifndef BLACKBOX_VOLANTA_H
+#define BLACKBOX_VOLANTA_H
+
+
+class volanta
+{
+};
+
+
+#endif //BLACKBOX_VOLANTA_H
